972f1917048cbc2291ee368aa1d7af4164d939a9
신혜원 <hehe2you@gmail.com>
2022-05-23 15:16:57 +0900

    movie

--- final_pjt_back/movie/urls.py
+++ final_pjt_back/movie/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('review/search/', views.give_movie_data),
     path('popular/', views.main_movie),
     path('<int:movie_pk>/', views.movie_detail),
-    path('<int:movie_pk>/score', views.score_add_change_delete),
+    path('<int:movie_pk>/score/', views.score_add_change_delete),
     path('recommends/representative/', views.inital_movie),
     path('recommends/', views.recommends),
 ]
